2be96442cb00970d82cdf4dfde913b236a918426
PonomarevaAnastasia <116709050+PonomarevaAnastasia@users.noreply.github.com>
2023-04-09 22:25:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,34 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T arr[100];
+    int obj;
+ public:
+    Tstack() :obj(-1) {}
+    void push(T value) {
+      if (isFull()) {
+        throw std::string("Stack is full");
+      } else {
+        arr[++obj] = value;
+      }
+    }
+      const T& pop() {
+      if (isEmpty()) {
+        throw std::string("Stack is empty");
+      } else {
+        return arr[obj--];
+      }
+    }
+    bool isEmpty()const {
+      return obj == -1;
+    }
+    bool isFull()const {
+      return obj == size - 1;
+    }
+    const T& get()const {
+      return arr[obj];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_

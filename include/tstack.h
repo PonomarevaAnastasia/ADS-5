29c20feb50364f6070f0917a877985c6d623e8d1
PonomarevaAnastasia <116709050+PonomarevaAnastasia@users.noreply.github.com>
2023-04-16 18:31:39 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,34 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int size = 0>
 class TStack {
-  // добавьте код стека
+ private:
+    T arr[size] = {0};
+    int obj;
+
+ public:
+    TStack() {
+        obj = -1;
+    }
+    void Push(const T& value) {
+        if (!isFull()) {
+            arr[++obj] = value;
+        }
+    }
+    T get() const {
+        return arr[obj];
+    }
+    T pop() {
+        return arr[obj--];
+    }
+    int GetTop() const { return top; }
+    bool isEmpty() const {
+        return obj == -1;
+    }
+    bool isFull() const {
+        return obj == size;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
